90b42a21646a3330859c2ee27d461f0ce6914077
Cody Hiar <cody.hiar@investopedia.com>
2015-10-27 22:01:50 -0600

    Cleaned up the fab file

--- fabfile.py
+++ fabfile.py
@@ -1,47 +1,51 @@
-from fabric.api import * #Fabric
-from time import gmtime, strftime #Timestamping files
-import string # For split() function
-import os # filename stuff
+# -*- coding: utf-8 -*-
+'''
+Fabfile for dealing with some simple vagrant commands
+'''
 
-# Defining the vagrant environment
+# Import libs
+from fabric.api import * 
+from time import gmtime, strftime
+import string
+import os
+
 def vagrant():
-    # change from the default user to 'vagrant'
+    '''
+    Method for defining the vagrant environment
+    '''
     env.user = 'vagrant'
-    # connect to the port-forwarded ssh
     env.hosts = ['127.0.0.1:2222']
-    # use vagrant ssh key
     result = local('vagrant ssh-config | grep IdentityFile', capture=True)
     env.key_filename = result.split()[1]
 
-# Dump all of the mysql databases from vagrant
 def mysql_export():
-    # Get all the databases
+    '''
+    Dump all the MySQL databases in vagrant to ~/Desktop
+    '''
     databases = run("mysql --user=root --password=root -e 'SHOW DATABASES;' | tr -d '| ' | grep -v Database") 
-    # Turn multi-line output into array of words
     databases = databases.split('\n')
     current_date = strftime("%Y_%m_%d", gmtime())
     local_dir = '~/Desktop/vagrant_mysql_dump_' + current_date
-    # Make the directory
     local('mkdir -p %s' % local_dir)
     for database in databases:
-    # Remove white space
         database = database.strip()
-        # Ignore the default databases
         if database not in ['mysql', 'performance_schema', 'information_schema']:
             remote_file = '%s.sql' % database
             run('mysqldump -u root -proot --result-file=%s %s' % (remote_file, database))
             get(remote_path=remote_file, local_path=local_dir)
             run('rm %s' % remote_file)
 
-
-# Import a bunch of sql files from a folder into vagrant
 def mysql_import(local_path):
+    '''
+    If you have a bunch of .sql files inside of a folder 
+    this import will bring all of the files into the vagrant 
+    mysql server based on filename
+    '''
     if prompt('Are you sure you wish to import databases? This may overwrite if duplicates exist.'):
         mysql_files = local("ls %s" % local_path, capture=True)
         mysql_files = mysql_files.split('\n')
         print "Here they come"
         for import_file in mysql_files:
-            #get rid of any white space
             import_file = import_file.strip()
             local_file = '%s/%s' % (local_path, import_file)
             put(local_file, '/home/vagrant/')
@@ -49,7 +53,6 @@ def mysql_import(local_path):
             run('mysql -u root -proot %s < %s' % (os.path.splitext(import_file)[0], import_file))
             run('rm %s' % import_file)
 
-# Issues a y/n prompt to the user
 def prompt(query):
     print('\n%s [y/n]' % query)
     val = raw_input()
